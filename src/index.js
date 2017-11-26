f5a8f2077a85ab8f6734bc323d5379dc98c485b7
Navid Kalaei <navidkalaei@gmail.com>
2017-11-26 16:46:04 +0330

    feat: add style

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import './vendor/bootstrap/css/bootstrap.min.css';
+import './vendor/font-awesome/css/font-awesome.min.css';
+import './css/fonts/lora.css';
+import './css/fonts/openSans.css';
+import './css/clean-blog.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
