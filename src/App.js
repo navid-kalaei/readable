5275b6c606500ebea94aec62824e2d7bc3f7e56b
Navid Kalaei <navidkalaei@gmail.com>
2017-11-26 17:32:37 +0330

    feat: add Header component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import Nav from './components/Nav';
-import homeBackground from './img/home-bg.jpg';
+import Header from './components/Header';
 
 class App extends Component {
     render() {
@@ -8,20 +8,7 @@ class App extends Component {
             <div>
                 <Nav/>
 
-                {/*Page Header*/}
-                <header className="masthead" style={{backgroundImage: `url(${homeBackground})`}}>
-                    <div className="overlay"></div>
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-lg-8 col-md-10 mx-auto">
-                                <div className="site-heading">
-                                    <h1>Clean Blog</h1>
-                                    <span className="subheading">A Blog Theme by Start Bootstrap</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </header>
+                <Header/>
 
                 {/*Main Content */}
                 <div className="container">
