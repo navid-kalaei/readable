adf5d1eed965e6cd321bf470c3fe58fcb5572df0
Navid Kalaei <navidkalaei@gmail.com>
2017-11-26 17:37:31 +0330

    feat: add Footer component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Nav from './components/Nav';
 import Header from './components/Header';
 import Pager from './components/Pager';
+import Footer from './components/Footer';
 
 class App extends Component {
     render() {
@@ -79,42 +80,7 @@ class App extends Component {
 
                 <hr/>
 
-                {/*Footer*/}
-                <footer>
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-lg-8 col-md-10 mx-auto">
-                                <ul className="list-inline text-center">
-                                    <li className="list-inline-item">
-                                        <a href="#">
-                  <span className="fa-stack fa-lg">
-                    <span className="fa fa-circle fa-stack-2x"></span>
-                    <span className="fa fa-twitter fa-stack-1x fa-inverse"></span>
-                  </span>
-                                        </a>
-                                    </li>
-                                    <li className="list-inline-item">
-                                        <a href="#">
-                  <span className="fa-stack fa-lg">
-                    <span className="fa fa-circle fa-stack-2x"></span>
-                    <span className="fa fa-facebook fa-stack-1x fa-inverse"></span>
-                  </span>
-                                        </a>
-                                    </li>
-                                    <li className="list-inline-item">
-                                        <a href="#">
-                  <span className="fa-stack fa-lg">
-                    <span className="fa fa-circle fa-stack-2x"></span>
-                    <span className="fa fa-github fa-stack-1x fa-inverse"></span>
-                  </span>
-                                        </a>
-                                    </li>
-                                </ul>
-                                <p className="copyright text-muted">Copyright &copy; Your Website 2017</p>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
+                <Footer/>
             </div>
         );
     }
---
+++ src/components/Footer.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+export default () => (
+    <footer>
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-8 col-md-10 mx-auto">
+                    <ul className="list-inline text-center">
+                        <li className="list-inline-item">
+                            <a href="#">
+                  <span className="fa-stack fa-lg">
+                    <span className="fa fa-circle fa-stack-2x"></span>
+                    <span className="fa fa-twitter fa-stack-1x fa-inverse"></span>
+                  </span>
+                            </a>
+                        </li>
+                        <li className="list-inline-item">
+                            <a href="#">
+                  <span className="fa-stack fa-lg">
+                    <span className="fa fa-circle fa-stack-2x"></span>
+                    <span className="fa fa-facebook fa-stack-1x fa-inverse"></span>
+                  </span>
+                            </a>
+                        </li>
+                        <li className="list-inline-item">
+                            <a href="#">
+                  <span className="fa-stack fa-lg">
+                    <span className="fa fa-circle fa-stack-2x"></span>
+                    <span className="fa fa-github fa-stack-1x fa-inverse"></span>
+                  </span>
+                            </a>
+                        </li>
+                    </ul>
+                    <p className="copyright text-muted">Copyright &copy; Your Website 2017</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+);
