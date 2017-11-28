c7902bb9d5db1d75b1a799859e3bc704c242abe8
Navid Kalaei <navidkalaei@gmail.com>
2017-11-28 14:30:06 +0330

    fix: responseResolved api

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,25 +6,32 @@ const headers = {
 
 const getUrl = (route) => `${BASE_URL}${route}`;
 
-const responseResolved = (res) => {
+const responseResolved = (res) => (
     res.json().then((data) => {
         if (data.error) {
             return Promise.reject(data)
         }
 
+        if (!Array.isArray(data) && !Object.keys(data).length) {
+            return Promise.reject({
+                error: "No data",
+                data
+            });
+        }
+
         return data;
     })
-};
+);
 
 export const fetchCategories = () => (
-    fetch(getUrl("/categories"), { headers }).then(responseResolved)
+    fetch(getUrl("/categories"), {headers}).then(responseResolved)
 );
 
 export const fetchPosts = (category = null) => {
     const url = category ? `/${category}/posts` : "/posts";
-    return fetch(getUrl(url), { headers }).then(responseResolved);
+    return fetch(getUrl(url), {headers}).then(responseResolved);
 };
 
 export const fetchPost = (id) => (
-    fetch(getUrl(`/posts/${id}`), { headers }).then(responseResolved)
+    fetch(getUrl(`/posts/${id}`), {headers}).then(responseResolved)
 );
