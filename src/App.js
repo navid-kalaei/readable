2becced84cf53c8212b5989c67320ea0e932944e
Navid Kalaei <navidkalaei@gmail.com>
2017-11-28 14:32:45 +0330

    feat: connect App to store

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import Nav from './components/Nav';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import PostPreview from './components/PostPreview';
+import { fetchCategories } from './actions/categories';
 
 class App extends Component {
 
+    componentWillMount() {
+        this.props.fetchCategories();
+    }
+
     render() {
+        console.log(this.props);
         return (
             <div>
                 <Nav/>
@@ -35,4 +43,17 @@ class App extends Component {
     }
 }
 
-export default App;
+
+const mapStateToProps = (state) => ({});
+
+const mapDispatchToProps = (dispatch) => (
+    bindActionCreators(
+        {
+            fetchCategories
+        },
+        dispatch
+    )
+);
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
