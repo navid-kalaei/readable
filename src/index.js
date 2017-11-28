3b5c377d4b9bd9491920e6325f58ae14de5d5a84
Navid Kalaei <navidkalaei@gmail.com>
2017-11-28 14:32:04 +0330

    feat: add store to the main index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './vendor/bootstrap/css/bootstrap.min.css';
@@ -7,6 +8,11 @@ import './vendor/font-awesome/css/font-awesome.min.css';
 import './css/fonts/lora.css';
 import './css/fonts/openSans.css';
 import './css/clean-blog.min.css';
+import store from "./config/store";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+    , document.getElementById('root'));
 registerServiceWorker();
