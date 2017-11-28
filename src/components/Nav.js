a6f78b063e40620ad06d851da654b16d4b805c01
Navid Kalaei <navidkalaei@gmail.com>
2017-11-28 19:00:24 +0330

    fix: home link

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
+
 class Nav extends Component {
 
     render() {
@@ -9,7 +10,7 @@ class Nav extends Component {
         return (
             <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
                 <div className="container">
-                    <a className="navbar-brand" href="index.html">Start Bootstrap</a>
+                    <a className="navbar-brand" href="index.html">Home</a>
                     <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
                             data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
                             aria-label="Toggle navigation">
