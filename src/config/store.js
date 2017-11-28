b91b827c1ee0b9808bdb8ba2521985a8204911cc
Navid Kalaei <navidkalaei@gmail.com>
2017-11-28 16:12:12 +0330

    feat: add logger and redux devtool to store

--- src/config/store.js
+++ src/config/store.js
@@ -1,10 +1,18 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 import reducer from '../reducers';
 
-const middleware = [thunk];
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer, applyMiddleware(...middleware));
+const middleware = [thunk, logger];
+
+const store = createStore(
+    reducer,
+    composeEnhancers(
+        applyMiddleware(...middleware)
+    )
+);
 
 export default store;
