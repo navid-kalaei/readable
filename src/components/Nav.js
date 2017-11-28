d9849d78c8b9a344f44d2cc06d6a9b3f3fd867be
Navid Kalaei <navidkalaei@gmail.com>
2017-11-28 18:27:16 +0330

    feat: conver components/Nav to class for using its props

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,31 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
-export default () => (
-    <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-        <div className="container">
-            <a className="navbar-brand" href="index.html">Start Bootstrap</a>
-            <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
-                    data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                Menu
-                <i className="fa fa-bars"></i>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarResponsive">
-                <ul className="navbar-nav ml-auto flex-row-reverse">
-                    <li className="nav-item">
-                        <a className="nav-link" href="index.html">Home</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="about.html">About</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="post.html">Sample Post</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="post.html">Add Post</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-);
+class Nav extends Component {
+
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+                <div className="container">
+                    <a className="navbar-brand" href="index.html">Start Bootstrap</a>
+                    <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+                            data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+                            aria-label="Toggle navigation">
+                        Menu
+                        <i className="fa fa-bars"></i>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarResponsive">
+                        <ul className="navbar-nav ml-auto flex-row-reverse">
+                            <li className="nav-item">
+                                <a className="nav-link" href="index.html">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="about.html">About</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="post.html">Sample Post</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="post.html">Add Post</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        )
+    }
+}
+
+
+const mapStateToProps = ({categories}) => ({
+    categories
+});
+
+export default connect(mapStateToProps)(Nav);
