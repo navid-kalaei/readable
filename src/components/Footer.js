54a1a3c6489d37dbee4170e29dbadeabbe466fd2
Navid Kalaei <navidkalaei@gmail.com>
2017-11-28 14:40:33 +0330

    fix: social media links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,26 +7,26 @@ export default () => (
                 <div className="col-lg-8 col-md-10 mx-auto">
                     <ul className="list-inline text-center">
                         <li className="list-inline-item">
-                            <a href="#">
+                            <a href="https://twitter.com">
                   <span className="fa-stack fa-lg">
-                    <span className="fa fa-circle fa-stack-2x"></span>
-                    <span className="fa fa-twitter fa-stack-1x fa-inverse"></span>
+                    <span className="fa fa-circle fa-stack-2x"/>
+                    <span className="fa fa-twitter fa-stack-1x fa-inverse"/>
                   </span>
                             </a>
                         </li>
                         <li className="list-inline-item">
-                            <a href="#">
+                            <a href="https://facebook.com">
                   <span className="fa-stack fa-lg">
-                    <span className="fa fa-circle fa-stack-2x"></span>
-                    <span className="fa fa-facebook fa-stack-1x fa-inverse"></span>
+                    <span className="fa fa-circle fa-stack-2x"/>
+                    <span className="fa fa-facebook fa-stack-1x fa-inverse"/>
                   </span>
                             </a>
                         </li>
                         <li className="list-inline-item">
-                            <a href="#">
+                            <a href="https://github.com/navid-kalaei">
                   <span className="fa-stack fa-lg">
-                    <span className="fa fa-circle fa-stack-2x"></span>
-                    <span className="fa fa-github fa-stack-1x fa-inverse"></span>
+                    <span className="fa fa-circle fa-stack-2x"/>
+                    <span className="fa fa-github fa-stack-1x fa-inverse"/>
                   </span>
                             </a>
                         </li>
