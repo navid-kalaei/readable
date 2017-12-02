cce559858f308f71e6d37bfc784b71447bb740c3
Navid Kalaei <navidkalaei@gmail.com>
2017-12-02 17:37:03 +0330

    feat: add voteComment action

--- src/actions/comments.js
+++ src/actions/comments.js
@@ -1,6 +1,7 @@
 import * as api from '../utils/api';
 
 export const FETCH_COMMENTS = 'FETCH_COMMENTS';
+export const UPDATE_COMMENT = 'UPDATE_COMMENT';
 
 export const fetchComments = (postId = null) => (dispatch) => (
     api.fetchComments(postId).then((comments) => (
@@ -14,3 +15,12 @@ export const fetchComments = (postId = null) => (dispatch) => (
     ))
 );
 
+export const voteComment = ({id, vote}) => (dispatch) => (
+    api.voteComment({id, vote}).then((comment) => (
+            dispatch({
+                type: UPDATE_COMMENT,
+                comment
+            })
+        )
+    )
+);
